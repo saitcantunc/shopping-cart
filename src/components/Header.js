1b6a0a35da8eec6e777d93c102f16788a692f765
saitcantunc <tuncsaitcan@gmail.com>
2022-02-08 07:43:42 +0300

    Products and Header styles done

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,19 @@ const Header = () => {
 	const context = useContext(BookContext);
 
 	return (
-		<div className='header'>
-			<div className='home'>
-				<Link to='/'>
-					<i class='fas fa-home'></i>
-				</Link>
-			</div>
-			<div className='title'>Cart App</div>
-			<div className='cart'>
-				<Link to='/cart'>
-					<i class='fas fa-shopping-cart'> ({context.totalCartCount})</i>
-				</Link>
+		<div className='header-container'>
+			<div className='header'>
+				<div className='home'>
+					<Link to='/'>
+						<i class='fas fa-home'></i>
+					</Link>
+				</div>
+				<div className='title'>Cart App</div>
+				<div className='cart'>
+					<Link to='/cart'>
+						<i class='fas fa-shopping-cart'> ({context.totalCartCount})</i>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
